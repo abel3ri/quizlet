5a337a3c79650f24da9d50379395a146f5128334
abel3ri <abelmerete@yahoo.com>
2024-05-25 00:50:12 +0300

    structure app and setup app state with useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { useReducer } from "react";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+
+const initalState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "error" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Unknown action");
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [{ questions, status }, dispatch] = useReducer(reducer, initalState);
+  useEffect(() => {
+    async function fakeAPIDemo() {
+      try {
+        const res = await axios.get("http://localhost:8000/questions/");
+        return dispatch({ type: "dataReceived", payload: res.data });
+      } catch (error) {
+        return dispatch({ type: "dataFailed" });
+      }
+    }
+    fakeAPIDemo();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      {console.log(status)}
+      <Header />
+      <Main>
+        {status == "loading" && <Loader />}
+        {status == "error" && <Error />}
+      </Main>
+    </div>
+  );
 }
 
-export default App
+export default App;
