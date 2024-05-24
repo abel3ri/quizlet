3b3e533f1667e0a0cb97a5abac6560df398bcda1
abel3ri <abelmerete@yahoo.com>
2024-05-25 00:46:02 +0300

    add loader and error components

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="status-container">
+      <h6>
+        <span>❌</span> There was an error fetching questions <span>❌</span>
+      </h6>
+    </div>
+  );
+}
---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="status-container">
+      <div className="loader"></div>
+    </div>
+  );
+}
