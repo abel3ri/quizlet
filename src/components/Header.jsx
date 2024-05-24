ff0e03b662ca46cf67b0c9b83aac9ebead9f0eb7
abel3ri <abelmerete@yahoo.com>
2024-05-25 00:48:40 +0300

    structure app, setup state with useReducer hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,7 @@
+import "../styles/Header.css";
 import logo from "../assets/quiz_logo.png";
-import axios from "axios";
-import { useEffect } from "react";
+
 export default function Header() {
-  useEffect(() => {
-    async function fakeAPIDemo() {
-      const res = await axios.get("http://localhost:3000/posts/");
-      console.log(res.data);
-    }
-
-    fakeAPIDemo();
-  }, []);
-
   return (
     <header className="app-header">
       <img src={logo} alt="quiz logo" />
