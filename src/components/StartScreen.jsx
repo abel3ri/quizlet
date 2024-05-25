1830a52efe0b6a056618f3911467aa5abe06f77a
abel3ri <abelmerete@yahoo.com>
2024-05-25 17:41:32 +0300

    add start quiz btn

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,13 +1,20 @@
 import "../styles/StartScreen.css";
 
-export default function StartScreen({ questions }) {
+export default function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start-screen">
       <h1 className="headline">Welcome to Quizlet!</h1>
       <h2 className="sub-headline">
-        {questions.length} questions to test your React mastery
+        {numQuestions} questions to test your React mastery
       </h2>
-      <button className="btn start-btn">Let's Start</button>
+      <button
+        onClick={() => {
+          dispatch({ type: "start" });
+        }}
+        className="btn start-btn"
+      >
+        Let's Start
+      </button>
     </div>
   );
 }
