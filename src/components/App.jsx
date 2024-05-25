20a36bffbf2f11f36e584d823d03bac49b16ab9e
abel3ri <abelmerete@yahoo.com>
2024-05-25 23:52:10 +0300

    reorganize components, add additional states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,10 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import NextButton from "./NextButton";
+import Timer from "./Timer";
 
 const initalState = {
   questions: [],
@@ -16,7 +20,11 @@ const initalState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  remainingSeconds: 10,
 };
+
+const SECS_PER_QUESTION = 30;
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -27,7 +35,11 @@ const reducer = (state, action) => {
       return { ...state, status: "error" };
 
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        remainingSeconds: SECS_PER_QUESTION * state.questions.length,
+      };
 
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -43,16 +55,40 @@ const reducer = (state, action) => {
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
 
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+        answer: null,
+      };
+
+    case "restartQuiz":
+      return {
+        ...initalState,
+        status: "active",
+        questions: state.questions,
+        highscore: state.highscore,
+      };
+
+    case "tick":
+      return {
+        ...state,
+        remainingSeconds: state.remainingSeconds - 1,
+        status: state.remainingSeconds == 0 ? "finished" : state.status,
+      };
+
     default:
       throw new Error("Unknown action");
   }
 };
 
 function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initalState
-  );
+  const [
+    { questions, status, index, answer, points, highscore, remainingSeconds },
+    dispatch,
+  ] = useReducer(reducer, initalState);
 
   const numQuestions = questions.length;
   const totalPoints = questions.reduce((acc, cur) => {
@@ -60,7 +96,7 @@ function App() {
   }, 0);
 
   useEffect(() => {
-    async function fakeAPIDemo() {
+    async function fetchQuestions() {
       try {
         const res = await axios.get("http://localhost:8000/questions/");
         return dispatch({ type: "dataReceived", payload: res.data });
@@ -68,7 +104,7 @@ function App() {
         return dispatch({ type: "dataFailed" });
       }
     }
-    fakeAPIDemo();
+    fetchQuestions();
   }, []);
 
   return (
@@ -97,6 +133,29 @@ function App() {
               answer={answer}
             />
           </>
+        )}
+        <Footer>
+          {status == "active" && (
+            <Timer dispatch={dispatch} remainingSeconds={remainingSeconds} />
+          )}
+
+          {status == "active" && (
+            <NextButton
+              answer={answer}
+              dispatch={dispatch}
+              index={index}
+              numQuestion={numQuestions}
+            />
+          )}
+        </Footer>
+
+        {status == "finished" && (
+          <FinishScreen
+            points={points}
+            totalPoints={totalPoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
